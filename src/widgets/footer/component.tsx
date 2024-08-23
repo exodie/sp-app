59fac69510e4f218cdd748283b49a161ebe6b166
im. <83586607+exodie@users.noreply.github.com>
2024-08-24 00:05:09 +0400

    refactor: move footer as component
    - Также добавил кнопку смены текущей цветовой темы в этот компонент

---
+++ src/widgets/footer/component.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+import { SwitchThemes } from '@/shared/ui/switch-themes'
+import { Paragraph } from '@/shared/ui/typography'
+
+export function Footer() {
+  return (
+    <footer className="flex flex-row items-center justify-between w-full space-x-2 px-4 md:px-6 py-2">
+      <div className="flex flex-row items-center space-x-2">
+        <Paragraph>Разработано: </Paragraph>
+        <Link href={'https://github.com/exodie'}>@exodie</Link>
+        <Link href={'https://github.com/bracerr'}>@bracerr</Link>
+      </div>
+
+      <div className="ml-auto mr-0">
+        <SwitchThemes />
+      </div>
+    </footer>
+  )
+}
--- src/widgets/layout/index.tsx
+++ src/widgets/layout/index.tsx
@@ -1,7 +1,6 @@
 import { PropsWithChildren } from 'react'
 
-import Link from 'next/link'
-
+import { Footer } from '../footer/component'
 import { Header } from '../header'
 
 export function Layout({ children }: PropsWithChildren) {
@@ -11,10 +10,7 @@ export function Layout({ children }: PropsWithChildren) {
       <main className="flex-1 flex flex-col justify-between px-4 md:px-6 py-2 my-16">
         {children}
       </main>
-      <footer className="flex w-full space-x-2 px-4 md:px-6 py-2">
-        <Link href={'https://github.com/exodie'}>@exodie</Link>
-        <Link href={'https://github.com/bracerr'}>@bracerr</Link>
-      </footer>
+      <Footer />
     </>
   )
 }
